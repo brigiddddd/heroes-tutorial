0944bba917ecd78842dc4dd001a59da27749e407
brigid <brigid.halling@gmail.com>
2017-11-03 14:17:38 -0500

    Add angular-in-memory-web-api to package.json
    Update changes that went with that (removing .data)

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { HeroService } from './hero.service'
 
 
 @Component({
@@ -15,6 +15,12 @@ export class DashboardComponent implements OnInit {
 
     ngOnInit(): void {
         this.heroService.getHeroes()
-            .then(heroes => this.heroes = heroes.slice(1, 5));
+            .then(heroes => {
+              if (heroes) {
+                this.heroes = heroes.slice(1, 5);
+              } else {
+                console.log('Could not find any heroes');
+              }
+            });
     }
 }
