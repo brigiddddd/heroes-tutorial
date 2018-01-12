4bd6f884081918a4a4e9d0c9d5aac12f00a5978c
brigid <brigid.halling@gmail.com>
2018-01-12 12:54:05 -0600

    Milestone 1: Getting started with the router.
    (Note: does not work)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './not-found.component';
 import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
@@ -5,12 +6,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CrisisListComponent } from './crisis/crisis-list.component';
+import { HeroListComponent } from './heroes/hero-list.component';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     CoreModule.forRoot({ userName: 'Miss Marple' }),
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
+    // AppRoutingModule
   ],
   declarations: [AppComponent, PageNotFoundComponent],
   bootstrap: [AppComponent]
